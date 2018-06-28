8921a41392e9d4a5e233036aec1037ca0351e5d9
Max Huijsdens <m.huijsdens@gmail.com>
2018-06-29 00:06:12 +0200

    Angular basis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { WerknemerComponent } from './werknemer/werknemer.component';
+
+const routes: Routes = [
+  {path: 'werknemer', component:WerknemerComponent},
+  {path: '', component:WerknemerComponent}
+
+];
+
+
 
 @NgModule({
   declarations: [
@@ -15,6 +25,8 @@ import { WerknemerComponent } from './werknemer/werknemer.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
